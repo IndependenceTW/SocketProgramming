92e6bd76f0341d8abae42db9fe3f98acb1835f08
IndependenceTW <danny900330@gmail.com>
2021-12-03 19:52:21 +0800

    20211203 a simple response

--- server.py
+++ server.py
@@ -7,26 +7,47 @@ SERVERIP = "127.0.0.1"
 SERVERPORT = 48763
 ADDR = (SERVERIP, SERVERPORT)
 FORMAT = 'utf-8'
-DISCONNECT_MESSAGE = "!DISCONNECT"
+RESPONSE_MSG = "HTTP/1.1 200 OK\r\n"
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(ADDR)
 
+def handle_request(msg):
+    if(len(msg) == 0):
+        return RESPONSE_MSG
+
+    msg = msg.split()
+    print(f"{msg[1]}")
+    
+    response_msg = RESPONSE_MSG + "Content-Type: text/html\r\n\r\n"
+    print(response_msg)
+    file = open("./pages/index.html")
+    for line in file:
+        response_msg += line
+    file.close()
+    print(response_msg)
+    
+    return response_msg
 
 def handle_client(conn, addr):
-    print(f"[SERVER MSG] {addr} connected successful")
-    connected = True
+    try:
+        print(f"[SERVER MSG] {addr} connected successful")
+        connected = True
 
-    while connected:
-        msg = conn.recv(BUFFERLENGTH).decode(FORMAT)
-        if(msg == DISCONNECT_MESSAGE):
-            connected = False
-        print(f"[FROM {addr}] {msg}")
-        conn.send(("GET Your Message: " + msg ).encode(FORMAT))
+        # while connected:
+        req_msg = conn.recv(BUFFERLENGTH).decode(FORMAT)
+        # if(msg == DISCONNECT_MESSAGE):
+        #     connected = False
+        print(f"[FROM {addr} request]\n {req_msg}")
+        res_msg = handle_request(req_msg)
+        conn.send(res_msg.encode(FORMAT))
 
-    conn.close()
-    print(f"[SERVER MSG] {addr} disconnected")
-        
+        conn.close()
+        print(f"[SERVER MSG] {addr} disconnected")
+    except KeyboardInterrupt:
+        print("Bye")
+        return
+
 
 
 def start():
@@ -40,8 +61,10 @@ def start():
             thread.start()
             print(f"[SERVER MSG] now active connections {threading.active_count() - 1}")
     except KeyboardInterrupt:
-        print("[SERVER MSG] Bye")
+        print("[SERVER MSG] Server Down")
+        
         server_socket.close()
+        exit(0)
 
 print("[SERVER MSG] Server is starting...")
 start()
